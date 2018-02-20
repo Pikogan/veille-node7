21d3fd89f33434dbae469e8f98b768eeac3e001b
Camille Archambault <c.archambault01@gmail.com>
2018-02-20 15:19:50 -0500

    Début exercice 7

---
+++ mes_modules/peupler/tableaux.js
@@ -0,0 +1,14 @@
+"use strict";
+let tableau = [
+				"Montréal",
+				"Paris",
+				"Québec",
+				"Laval",
+				"Bruxelles",
+				"Ottawa",
+				"Calgary",
+				"Gaspé",
+				"Gatineau"
+				];
+
+module.exports = tableau;
