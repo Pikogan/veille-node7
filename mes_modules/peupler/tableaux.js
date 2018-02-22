61e689e3b0810e26c75d41c2f7e85af497b53013
Camille Archambault <c.archambault01@gmail.com>
2018-02-22 08:38:59 -0500

    Modification des fichiers ejs

--- mes_modules/peupler/tableaux.js
+++ mes_modules/peupler/tableaux.js
@@ -3,8 +3,118 @@
  */
 
 'use strict';
+let tableaux = {
+   tabNom: [ 
+               'Martin',
+               'Marchand',
+               'Trudeau',
+               'Lafontaine',
+               'Gravel',
+               'Huberdeau',
+               'Massé',
+               'St-Pierre',
+               'Dandurand',
+               'Hamel',
+               'Larose',
+               'Ambroise',
+               'Kuvel',
+               'Lamarre',
+               'Sorel',
+               'Frapier',
+               'Archer',
+               'Dejoie',
+               'Moïse',
+               'Fortin',
+               'Stien',
+               'Gagnon',
+               'Chédiac',
+               'Roy',
+               'Gagner',
+               'Larue',
+               'Lafortune',
+               'Roffi',
+               'Cartier',
+               'Maisonneuve',
+               'Levesque',
+               'Talon',
+               'Bellemarre',
+               'Mercier'
+               ],
 
-let tabNom = [ 
+tabCourriel: [
+                  'hotmail.com',
+                  'vedeotron.com',
+                  'gmail.com',
+                  'yahoo.com',
+                  'cmaisonneuve.qc.ca',
+                  'webfaction.com',
+                  'cvsyc.com',
+                  'bell.ca',
+                  'umontreal.ca',
+                  'skillscanada.ca',
+                  'inforoutefpt.com'
+                  ],
+
+ville: [
+            'Montréal',
+            'Québec',
+            'Saskatshewan',
+            'Calgary',
+            'Edmonton',
+            'Laval',
+            'Ottawa',
+            'Gatineau',
+            'Gaspé',
+            'Hudson',
+            'New York'
+            ],
+
+tabPrenom: [
+                  'Luc',
+                  'Éric',
+                  'François',
+                  'Lyne',
+                  'carolle',
+                  'Sylvie',
+                  'Renée',
+                  'Mathieu',
+                  'Eddy',
+                  'Jean',
+                  'Paul',
+                  'Gustave',
+                  'Rose-Lyne',
+                  'Rose-Lise',
+                  'Rose',
+                  'Xavier',
+                  'Olivier',
+                  'Michel',
+                  'Jean-François',
+                  'Bob',
+                  'Gil',
+                  'Martin',
+                  'Pierre',
+                  'Maxime',
+                  'Léo',
+                  'Léo-Paul',
+                  'Paul',
+                  'Alix',
+                  'Alixe',
+                  'Chantal',
+                  'Christelle',
+                  'Katy',
+                  'Johanne',
+                  'Béatrice',
+                  'Noémie',
+                  'Coralie',
+                  'Pénéloppe',
+                  'Charlotte'
+                    ]
+}
+
+module.exports.tabNom = tableaux;
+
+
+/*let tabNom = [ 
                'Martin',
                'Marchand',
                'Trudeau',
@@ -110,4 +220,4 @@ let tabPrenom = [
                     ]
 
 module.exports.tabNom = tabNom;
-module.exports.tabPrenom = tabPrenom;
+module.exports.tabPrenom = tabPrenom;*/
