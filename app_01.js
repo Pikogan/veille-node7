a13c1344f93b043c65347d639b619ad152054b13
Camille Archambault <c.archambault01@gmail.com>
2018-02-22 08:18:43 -0500

    Changement du fichier tableaux

--- app_01.js
+++ app_01.js
@@ -1,125 +1,170 @@
+"use strict"
 const express = require('express');
+const fs = require("fs");
 const app = express();
-const bodyParser= require('body-parser')
-const MongoClient = require('mongodb').MongoClient // le pilote MongoDB
+const peupler = require('./mes_modules/peupler');
 const ObjectID = require('mongodb').ObjectID;
-
-app.set('view engine', 'ejs'); // générateur de template 
-app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'));
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+const MongoClient = require('mongodb').MongoClient // le pilote MongoDB
 
+// générateur de template
 app.set('view engine', 'ejs');
+let util = require("util");
 
-app.get('/',function(req, res){
+app.get('/membres', (req, res) => {
 
- res.render('accueil.ejs')
-})
+    let cursor = db.collection('adresse').find().toArray(function(err, resultat){
 
-app.get('/formulaire',function(req, res){
+         if (err) return console.log(err)
+         console.log('util = ' + util.inspect(resultat));
 
- res.render('formulaire.ejs')
-})
-
-////////////////////////////////////////////////////////////////////////////////
-
-app.get('/membres',function(req, res){
-
-var cursor = db.collection('db_exercice_6_NS').find().toArray(function(err, resultat){
- if (err) return console.log(err)
- // transfert du contenu vers la vue index.ejs (renders)
- // affiche le contenu de la BD
- res.render('gabarit.ejs', {ex_6: resultat})
-})
-})
-
-/////////////////////////////////////////////////////////////////////////////
-
-app.get('/ajouter', function (req, res) {
- // Preparer l'output en format JSON
-
- db.collection('db_exercice_6_NS').save(req.query, (err, result) => {
- if (err) return console.log(err)
- console.log()
-
-	
- res.redirect('/membres')
- })
-
+         //transfert du contenu vers la vue index.ejs (renders)
+         //affiche le contenu de la BD
+         res.render('membres.ejs', {adresses: resultat})
+     }) 
+ 
 });
 
-app.get('/suprimer/:id', (req, res) => {
- var id = req.params.id
- console.log(id)
- db.collection('db_exercice_6_NS').findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
+app.get('/', (req, res) => {
 
-if (err) return console.log(err)
- res.redirect('/membres')  // redirige vers la route qui affiche la collection
- })
+     console.log('accueil')
+     res.render('accueil.ejs')
+
 })
 
-///////////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////AJOUTER
+app.get('/ajouter', function (req, res) {
+
+    //Preparer l'output en format JSON
+    console.log('la route /traiter_get')
+
+    db.collection('adresse').save(req.query, (err, result) => {
+             
+        if (err) return console.log(err)
+        console.log('sauvegarder dans la BD')
+        res.redirect('/membres')
+    })
+})
+
+/////////////////////////////////////////////////////////SUPPRIMER
+app.get('/supprimer/:id', (req, res) => {
+
+    let id = req.params.id
+
+    db.collection('adresse').findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
+
+        if (err) return console.log(err)
+        res.redirect('/membres')
+    })
+})
+
+///////////////////////////////////////////////////////////TRIER
+app.get('/trier/:cle/:ordre', (req, res) => {
+    
+    let cle = req.params.cle
+    let ordre = (req.params.ordre == 'asc' ? 1 : -1)
+
+    let cursor = db.collection('adresse').find().sort(cle,ordre).toArray(function(err, resultat){
+
+        if(ordre == 1){
+
+            ordre = 'dsc';
+        }
+        else {
+
+            ordre = 'asc';
+        }
+
+        res.render('membres.ejs', {adresses: resultat, test:ordre})
+
+ 
+    })
+
+ })
+
+////////////////////////////////////////////////////////////MODIFIER
 app.post('/modifier', (req, res) => {
 
-//console.log('req.body' + req.body)
-console.log('***********************')
-console.log( req.body['_id']);
+    console.log(req)
+    
+        console.log('sauvegarde') 
 
- var oModif = {
- "_id": ObjectID(req.body['_id']), 
-  nom: req.body.nom,
- prenom:req.body.prenom, 
- telephone:req.body.telephone,
- email:req.body.email
- }
+        var oModif = {
 
- console.log(oModif);
+            "_id": ObjectID(req.body['_id']), 
+            nom: req.body.nom,
+            prenom:req.body.prenom, 
+            telephone:req.body.telephone,
+            courriel:req.body.courriel
 
-  db.collection('db_exercice_6_NS').save(oModif, (err, result) => {
- if (err) return console.log(err)
- console.log('sauvegarder dans la BD')
- res.redirect('/membres')
- })
-
- })
-
-////////////////////////////////////////////////////////////////////////////////////////////
-
- app.get('/trier/:cle/:ordre', (req, res) => {
-console.log(req.params.ordre)
- let cle = req.params.cle
- let ordre = (req.params.ordre == 'asc' ? 1 : -1)
- let cursor = db.collection('db_exercice_6_NS').find().sort(cle,ordre).toArray(function(err, resultat){
-
- ordre = (req.params.ordre == 1 ? 'desc' : 'asc')
-
- res.render('gabarit.ejs', {ex_6: resultat, cle_html:cle, ordre_html:ordre})
-
-})
-})
-
-////////////////////////////////////////////////////////////////////////////////////////////
-
-app.get('/peuplement', (req, res) => {
-    let peupler = require('./component/peuplement.js');
-    let nouvelleListe = peupler();
-    db.collection('adresse').insert(nouvelleListe, (err, enreg) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            peupler = "";
-            res.redirect('/');
         }
-    });
+
+
+        let util = require("util");
+        console.log('util = ' + util.inspect(oModif));
+    
+    db.collection('adresse').save(oModif, (err, result) => {
+
+        if (err) return console.log(err)
+        console.log('sauvegarder dans la BD')
+        res.redirect('/membres')
+
+    })
+
+ })
+
+////////////////////////////////////////////////////////////PEUPLER
+app.get('/peupler', function (req, res) {
+
+    let peuplement = peupler()
+    console.log(peuplement)
+
+    //Preparer l'output en format JSON
+    console.log('la route /traiter_get')
+
+    for(let i=0; i<peuplement.length; i++){
+
+         db.collection('adresse').save(peuplement[i], (err, result) => {
+             if (err) return console.log(err)
+             console.log('sauvegarder dans la BD')
+         })
+    }
+
+res.redirect('/membres')
+
 })
 
+
+////////////////////////////////////////////////////////////VIDER LA LISTE
+app.get('/viderlaliste', (req, res) => {
+
+    let id = req.params.id
+    console.log(id)
+
+    db.collection('adresse').remove( {}, (err, resultat) => {
+
+        if (err) return console.log(err)
+         res.redirect('/membres')
+    })
+})
+
+
+/////////////////////////////////////////////////////////FUB
 let db // variable qui contiendra le lien sur la BD
 
 MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
- if (err) return console.log(err)
- db = database.db('db_exercice_6_NS')
-// lancement du serveur Express sur le port 8081
- app.listen(8081, () => {
- console.log('connexion à la BD et on écoute sur le port 8081')
- })
+     
+     if (err) return console.log(err)
+     
+     db = database.db('carnet_adresse');
+
+    // lancement du serveur Express sur le port 8081
+     app.listen(8081, () => {
+
+        console.log('connexion à la BD et on écoute sur le port 8081')
+     
+     })
 })
--- mes_modules/peupler/tableaux.js
+++ mes_modules/peupler/tableaux.js
@@ -1,8 +1,113 @@
-"use strict";
-module.exports = {
-	tableauNom = ['Archambault','Martin','Tremblay','Gagnon','Girard','Leclerc','Landry','Bélanger'];
-	tableauPrenom = ['James','Eddy','Camille','William','Jessica','Sophie','Olivier','Steve'];
-	tableauAdresse = ['hotmail.com', 'hotmail.ca', 'gmail.com', 'aol.com', 'yahoo.com', 'yahoo.fr'];
-	tableauNum = [514,450,418,438];
+/*!
+ * tableaux.js
+ */
 
-}
+'use strict';
+
+let tabNom = [ 
+               'Martin',
+               'Marchand',
+               'Trudeau',
+               'Lafontaine',
+               'Gravel',
+               'Huberdeau',
+               'Massé',
+               'St-Pierre',
+               'Dandurand',
+               'Hamel',
+               'Larose',
+               'Ambroise',
+               'Kuvel',
+               'Lamarre',
+               'Sorel',
+               'Frapier',
+               'Archer',
+               'Dejoie',
+               'Moïse',
+               'Fortin',
+               'Stien',
+               'Gagnon',
+               'Chédiac',
+               'Roy',
+               'Gagner',
+               'Larue',
+               'Lafortune',
+               'Roffi',
+               'Cartier',
+               'Maisonneuve',
+               'Levesque',
+               'Talon',
+               'Bellemarre',
+               'Mercier'
+               ]
+
+let tabCourriel = [
+                  'hotmail.com',
+                  'vedeotron.com',
+                  'gmail.com',
+                  'yahoo.com',
+                  'cmaisonneuve.qc.ca',
+                  'webfaction.com',
+                  'cvsyc.com',
+                  'bell.ca',
+                  'umontreal.ca',
+                  'skillscanada.ca',
+                  'inforoutefpt.com']
+
+let ville = [
+            'Montréal',
+            'Québec',
+            'Saskatshewan',
+            'Calgary',
+            'Edmonton',
+            'Laval',
+            'Ottawa',
+            'Gatineau',
+            'Gaspé',
+            'Hudson',
+            'New York'
+            ]
+
+let tabPrenom = [
+                  'Luc',
+                  'Éric',
+                  'François',
+                  'Lyne',
+                  'carolle',
+                  'Sylvie',
+                  'Renée',
+                  'Mathieu',
+                  'Eddy',
+                  'Jean',
+                  'Paul',
+                  'Gustave',
+                  'Rose-Lyne',
+                  'Rose-Lise',
+                  'Rose',
+                  'Xavier',
+                  'Olivier',
+                  'Michel',
+                  'Jean-François',
+                  'Bob',
+                  'Gil',
+                  'Martin',
+                  'Pierre',
+                  'Maxime',
+                  'Léo',
+                  'Léo-Paul',
+                  'Paul',
+                  'Alix',
+                  'Alixe',
+                  'Chantal',
+                  'Christelle',
+                  'Katy',
+                  'Johanne',
+                  'Béatrice',
+                  'Noémie',
+                  'Coralie',
+                  'Pénéloppe',
+                  'Charlotte'
+                    ]
+
+module.exports.tabNom = tabNom;
+module.exports.tabPrenom = tabPrenom;
